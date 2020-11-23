c9c94e069df411317ae04ca84b570c778e7959e8
Eric Reichert <eric@reichertconsulting.com>
2020-11-22 21:07:46 -0500

    Added support for adding mementos.

--- src/bin/recallapi.rs
+++ src/bin/recallapi.rs
@@ -3,13 +3,13 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/world")]
-fn world() -> &'static str {
-    "hello world!"
+#[post("/mementos")]
+fn add_memento() -> &'static str {
+    "memento received!"
 }
 
 fn build_rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/hello", routes![world])
+    rocket::ignite().mount("/", routes![add_memento])
 }
 
 fn main() {
@@ -20,20 +20,15 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use crate::build_rocket;
-    use rocket::{local::Client, http::Status};
+    use rocket::{http::Status, local::Client};
 
     #[test]
-    fn get_hello_world_returns_200() {
+    fn post_new_memento_returns_200() {
         let rocket = build_rocket();
-        let client = Client::new(rocket).expect("invalid Rocket config detected");
+        let client = Client::new(rocket).expect("Invalid Rocket config detected.");
 
-        let response = client.get("/hello/world").dispatch();
+        let response = client.post("/mementos").dispatch();
 
         assert_eq!(response.status(), Status::Ok)
-    }
-
-    #[test]
-    fn this_is_the_second_test() {
-        unimplemented!();
     }
 }
